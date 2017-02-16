617e3673e40a11e8b9d79e46564a4c1cc7dd80c7
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-02-16 18:53:53 +0000

    Added intro and conclusion text for the start and end of the app

---
+++ lib/guide.rb
@@ -0,0 +1,18 @@
+class Guide
+
+  def initialize(path=nil)
+  end
+
+  def launch
+  end
+
+  def introducion
+    puts " <<<<< Welcome to Food Finder >>>>>>"
+    puts "An interactive guide to help you find those fantastic food flavours you're craving!"
+  end
+
+  def conclusion
+    puts "Farewell - come again soon!"
+  end
+
+end
